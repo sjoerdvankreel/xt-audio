c76c5ed32047d815540ce0d1beb792415d2ac618
sjoerd <sjoerdvankreel@gmail.com>
2016-08-21 15:33:18 +0200

    Undo latency monitoring sensitivity.

--- src/cli-gui/LatencyCallback.cs
+++ src/cli-gui/LatencyCallback.cs
@@ -29,46 +29,46 @@ namespace Xt {
                         switch (format.mix.sample) {
                             case XtSample.UInt8:
                                 ((byte[][])output)[0][f] = byteValue;
-                                pulseReceived = ((byte[][])input)[0][f] < 96 || ((byte[][])input)[0][f] > 160;
+                                pulseReceived = ((byte[][])input)[0][f] < 64 || ((byte[][])input)[0][f] > 192;
                                 break;
                             case XtSample.Int16:
                                 ((short[][])output)[0][f] = shortValue;
-                                pulseReceived = Math.Abs(((short[][])input)[0][f]) > short.MaxValue / 4;
+                                pulseReceived = Math.Abs(((short[][])input)[0][f]) > short.MaxValue / 2;
                                 break;
                             case XtSample.Int24:
                                 ((byte[][])output)[0][f * 3] = byteValue;
-                                pulseReceived = Math.Abs(((byte[][])input)[0][f * 3]) > byte.MaxValue / 4;
+                                pulseReceived = Math.Abs(((byte[][])input)[0][f * 3]) > byte.MaxValue / 2;
                                 break;
                             case XtSample.Int32:
                                 ((int[][])output)[0][f] = intValue;
-                                pulseReceived = Math.Abs(((int[][])input)[0][f]) > int.MaxValue / 4;
+                                pulseReceived = Math.Abs(((int[][])input)[0][f]) > int.MaxValue / 2;
                                 break;
                             case XtSample.Float32:
                                 ((float[][])output)[0][f] = floatValue;
-                                pulseReceived = Math.Abs(((float[][])input)[0][f]) > 0.25;
+                                pulseReceived = Math.Abs(((float[][])input)[0][f]) > 0.5;
                                 break;
                         }
                     } else {
                         switch (format.mix.sample) {
                             case XtSample.UInt8:
                                 ((byte[])output)[f * format.outputs] = byteValue;
-                                pulseReceived = ((byte[])input)[f * format.inputs] < 96 || ((byte[])input)[f * format.inputs] > 160;
+                                pulseReceived = ((byte[])input)[f * format.inputs] < 64 || ((byte[])input)[f * format.inputs] > 192;
                                 break;
                             case XtSample.Int16:
                                 ((short[])output)[f * format.outputs] = shortValue;
-                                pulseReceived = Math.Abs(((short[])input)[f * format.inputs]) > short.MaxValue / 4;
+                                pulseReceived = Math.Abs(((short[])input)[f * format.inputs]) > short.MaxValue / 2;
                                 break;
                             case XtSample.Int24:
                                 ((byte[])output)[f * format.outputs * 3] = byteValue;
-                                pulseReceived = Math.Abs(((byte[])input)[f * format.inputs * 3]) > byte.MaxValue / 4;
+                                pulseReceived = Math.Abs(((byte[])input)[f * format.inputs * 3]) > byte.MaxValue / 2;
                                 break;
                             case XtSample.Int32:
                                 ((int[])output)[f * format.outputs] = intValue;
-                                pulseReceived = Math.Abs(((int[])input)[f * format.inputs]) > int.MaxValue / 4;
+                                pulseReceived = Math.Abs(((int[])input)[f * format.inputs]) > int.MaxValue / 2;
                                 break;
                             case XtSample.Float32:
                                 ((float[])output)[f * format.outputs] = floatValue;
-                                pulseReceived = Math.Abs(((float[])input)[f * format.inputs]) > 0.25;
+                                pulseReceived = Math.Abs(((float[])input)[f * format.inputs]) > 0.5;
                                 break;
                         }
                     }
@@ -77,46 +77,46 @@ namespace Xt {
                         switch (format.mix.sample) {
                             case XtSample.UInt8:
                                 ((byte**)(IntPtr)output)[0][f] = byteValue;
-                                pulseReceived = ((byte**)(IntPtr)input)[0][f] < 96 || ((byte**)(IntPtr)input)[0][f] > 160;
+                                pulseReceived = ((byte**)(IntPtr)input)[0][f] < 64 || ((byte**)(IntPtr)input)[0][f] > 192;
                                 break;
                             case XtSample.Int16:
                                 ((short**)(IntPtr)output)[0][f] = shortValue;
-                                pulseReceived = Math.Abs(((short**)(IntPtr)input)[0][f]) > short.MaxValue / 4;
+                                pulseReceived = Math.Abs(((short**)(IntPtr)input)[0][f]) > short.MaxValue / 2;
                                 break;
                             case XtSample.Int24:
                                 ((byte**)(IntPtr)output)[0][f * 3] = byteValue;
-                                pulseReceived = Math.Abs(((byte**)(IntPtr)input)[0][f * 3]) > byte.MaxValue / 4;
+                                pulseReceived = Math.Abs(((byte**)(IntPtr)input)[0][f * 3]) > byte.MaxValue / 2;
                                 break;
                             case XtSample.Int32:
                                 ((int**)(IntPtr)output)[0][f] = intValue;
-                                pulseReceived = Math.Abs(((int**)(IntPtr)input)[0][f]) > int.MaxValue / 4;
+                                pulseReceived = Math.Abs(((int**)(IntPtr)input)[0][f]) > int.MaxValue / 2;
                                 break;
                             case XtSample.Float32:
                                 ((float**)(IntPtr)output)[0][f] = floatValue;
-                                pulseReceived = Math.Abs(((float**)(IntPtr)input)[0][f]) > 0.25;
+                                pulseReceived = Math.Abs(((float**)(IntPtr)input)[0][f]) > 0.5;
                                 break;
                         }
                     } else {
                         switch (format.mix.sample) {
                             case XtSample.UInt8:
                                 ((byte*)(IntPtr)output)[f * format.outputs] = byteValue;
-                                pulseReceived = ((byte*)(IntPtr)input)[f * format.inputs] < 96 || ((byte*)(IntPtr)input)[f * format.inputs] > 160;
+                                pulseReceived = ((byte*)(IntPtr)input)[f * format.inputs] < 64 || ((byte*)(IntPtr)input)[f * format.inputs] > 192;
                                 break;
                             case XtSample.Int16:
                                 ((short*)(IntPtr)output)[f * format.outputs] = shortValue;
-                                pulseReceived = Math.Abs(((short*)(IntPtr)input)[f * format.inputs]) > short.MaxValue / 4;
+                                pulseReceived = Math.Abs(((short*)(IntPtr)input)[f * format.inputs]) > short.MaxValue / 2;
                                 break;
                             case XtSample.Int24:
                                 ((byte*)(IntPtr)output)[f * format.outputs * 3] = byteValue;
-                                pulseReceived = Math.Abs(((byte*)(IntPtr)input)[f * format.inputs * 3]) > byte.MaxValue / 4;
+                                pulseReceived = Math.Abs(((byte*)(IntPtr)input)[f * format.inputs * 3]) > byte.MaxValue / 2;
                                 break;
                             case XtSample.Int32:
                                 ((int*)(IntPtr)output)[f * format.outputs] = intValue;
-                                pulseReceived = Math.Abs(((int*)(IntPtr)input)[f * format.inputs]) > int.MaxValue / 4;
+                                pulseReceived = Math.Abs(((int*)(IntPtr)input)[f * format.inputs]) > int.MaxValue / 2;
                                 break;
                             case XtSample.Float32:
                                 ((float*)(IntPtr)output)[f * format.outputs] = floatValue;
-                                pulseReceived = Math.Abs(((float*)(IntPtr)input)[f * format.inputs]) > 0.25;
+                                pulseReceived = Math.Abs(((float*)(IntPtr)input)[f * format.inputs]) > 0.5;
                                 break;
                         }
                     }
