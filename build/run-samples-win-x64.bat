7ad106b3f52b961de2e5ffe1be2f02749f5e0eab
sjoerd <sjoerdvankreel@gmail.com>
2020-05-24 17:03:23 +0200

    Update windows test scripts for netcore support.

--- build/run-samples-win-x64.bat
+++ build/run-samples-win-x64.bat
@@ -1,25 +1,25 @@
 @echo off
 set build=%CD%
 
-set /p ok="Start c++ sample (press return to continue)..."
+set /p ok="Starting c++ sample (press return to continue)..."
 cd ../dist/cpp-sample-win32-x64-static
 xt-cpp-sample.exe
 cd %build%
 echo C++ sample finished.
 
-set /p ok="Start netfx sample (press return to continue)..."
+set /p ok="Starting netfx sample (press return to continue)..."
 cd ../dist/cli-sample/net48
 xt-cli-sample.exe
 cd %build%
 echo Netfx sample finished.
 
-set /p ok="Start netcore sample (press return to continue)..."
+set /p ok="Starting netcore sample (press return to continue)..."
 cd ../dist/cli-sample/netcoreapp3.1
 xt-cli-sample.exe
 cd %build%
 echo Netcore sample finished.
 
-set /p ok="Start java sample (press return to continue)..."
+set /p ok="Starting java sample (press return to continue)..."
 cd ../dist/java-sample
 java -jar com.xt-audio.xt.sample-1.0.6.jar
 cd %build%
