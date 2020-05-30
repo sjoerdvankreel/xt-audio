5e5b914db4ca691dc885b17cdf0b957b0bf68425
sjoerd <sjoerdvankreel@gmail.com>
2020-05-30 16:11:35 +0200

    Update linux test scripts for netcore support.

--- build/run-samples-lin-x64.sh
+++ build/run-samples-lin-x64.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 
+read -p "Starting c++ sample (press return to continue)..."
 ../dist/cpp-sample-linux-x64-static/xt-cpp-sample
-cd ../dist/cli-sample/
+echo C++ sample finished.
+
+read -p "Starting netfx sample (press return to continue)..."
+cd ../dist/cli-sample/net48
 mono xt-cli-sample.exe
+cd ../../../build
+echo Netfx sample finished.
+
+read -p "Starting netcore sample (press return to continue)..."
+cd ../dist/cli-sample/netcoreapp3.1
+dotnet xt-cli-sample.dll
 cd ../../build
+echo Netcore sample finished.
+
+read -p "Starting java sample (press return to continue)..."
 cd ../dist/java-sample
 java -jar com.xt-audio.xt.sample-1.0.6.jar
 cd ../../build
+echo Java sample finished.
