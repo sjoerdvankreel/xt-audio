20e640ccc4b8c1e3fd1bbec61925cc013abf02ef
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2020-11-07 23:03:10 +0100

    Group stream parameters (cli) retake 3.

--- src/cli-sample/RenderSimple.cs
+++ src/cli-sample/RenderSimple.cs
@@ -3,37 +3,40 @@ using System.Threading;
 
 namespace Xt
 {
-	class State
-	{
-		public double phase = 0.0;
-		public readonly double Frequency = 440.0;
-		public readonly XtFormat Format = new XtFormat(new XtMix(44100, XtSample.Float32), new XtChannels(0, 0, 1, 0));
-	}
-
 	public class RenderSimple
 	{
-		static void Render(IntPtr s, in XtBuffer b, in XtTime time, ulong error, IntPtr u)
+		static float _phase;
+		const float Frequency = 440.0f;
+		static readonly XtMix Mix = new XtMix(44100, XtSample.Float32);
+		static readonly XtChannels Channels = new XtChannels(0, 0, 1, 0);
+		static readonly XtFormat Format = new XtFormat(Mix, Channels);
+
+		static float NextSample()
 		{
-			var adapter = XtAdapter.Get(s);
-			var state = (State)adapter.User;
-			using var io = adapter.IO();
-			for (int f = 0; f < b.frames; f++)
-			{
-				state.phase += state.Frequency / state.Format.mix.rate;
-				if (state.phase >= 1.0) state.phase = -1.0;
-				((float[])adapter.Output)[f] = (float)Math.Sin(2.0 * state.phase * Math.PI);
-			}
+			_phase += Frequency / Mix.rate;
+			if (_phase >= 1.0f) _phase = -1.0f;
+			return (float)Math.Sin(2.0 * _phase * Math.PI);
 		}
 
-		public static void Main(string[] args)
+		static void OnBuffer(IntPtr stream, in XtBuffer buffer, in XtTime time, ulong error, IntPtr _)
+		{
+			var adapter = XtAdapter.Get(stream);
+			adapter.LockBuffer(buffer);
+			float[] output = (float[])adapter.Output;
+			for (int f = 0; f < buffer.frames; f++) output[f] = NextSample();
+			adapter.UnlockBuffer(buffer);
+		}
+
+		public static void Main()
 		{
 			using XtAudio audio = new XtAudio(null, IntPtr.Zero, null);
-			XtService service = XtAudio.GetService(XtAudio.SetupToSystem(XtSetup.ConsumerAudio));
+			var system = XtAudio.SetupToSystem(XtSetup.ConsumerAudio);
+			XtService service = XtAudio.GetService(system);
 			if (service == null) return;
 			using XtDevice device = service.OpenDefaultDevice(true);
 			if (device?.SupportsFormat(Format) != true) return;
 			XtBufferSize size = device.GetBufferSize(Format);
-			using XtStream stream = device.OpenStream(Format, true, size.current, Render, null);
+			using XtStream stream = device.OpenStream(Format, true, size.current, OnBuffer, null);
 			using XtAdapter adapter = XtAdapter.Register(stream, null);
 			stream.Start();
 			Thread.Sleep(1000);
--- src/cli/XtAdapter.cs
+++ src/cli/XtAdapter.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 namespace Xt
 {
+	public struct XtBufferAdapter
+	{
+		public Array input;
+		public Array output;
+	}
+
 	public class XtAdapter : IDisposable
 	{
+		static readonly Dictionary<IntPtr, XtAdapter> _map = new Dictionary<IntPtr, XtAdapter>();
+
 		readonly object _user;
 		readonly XtStream _stream;
 		internal XtAdapter(XtStream stream, object user) => (_stream, _user) = (stream, user);
@@ -15,6 +24,12 @@ namespace Xt
 		public IDisposable IO() => null;
 		public XtStream Stream => _stream;
 		public static XtAdapter Get(IntPtr stream) => null;
-		public static XtAdapter Register(XtStream stream, object user) => new XtAdapter(stream, user);
+
+		public static XtAdapter Register(XtStream stream, object user)
+		{
+			var result = new XtAdapter(stream, user);
+			_map.Add(stream.Handle, result);
+			return result;
+		}
 	}
 }
