ecb8955fface9f58cd62a53d6969e746661baed4
sjoerd <sjoerdvankreel@gmail.com>
2020-11-12 19:29:13 +0100

    Merge stream parameters take 5.

--- src/cpp-sample/CaptureSimple.cpp
+++ src/cpp-sample/CaptureSimple.cpp
@@ -1,45 +1,35 @@
 #include <XtCpp.hpp>
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <unistd.h>
-#endif
+#include <cmath>
+#include <thread>
 #include <fstream>
 #include <iostream>
-#include <cmath>
 
-static const int SampleSize = 3;
-static const Xt::Format Format(Xt::Mix(44100, Xt::Sample::Int24), Xt::Channels(1, 0, 0, 0));
+static const Xt::Channels Channels(1, 0, 0, 0);
+static const Xt::Mix Mix(44100, Xt::Sample::Int24);
+static const Xt::Format Format(Mix, Channels);
 
-static void Capture(
-  const Xt::Stream& stream, const Xt::Buffer& buffer, void* user) {
-
-  if(buffer.frames > 0)
-    // Don't do this.
-    static_cast<std::ofstream*>(user)->write(static_cast<const char*>(buffer.input), buffer.frames * SampleSize);
+static void Capture(const Xt::Stream& stream, const Xt::Buffer& buffer, void* user) 
+{
+  auto os = static_cast<std::ofstream*>(user);
+  const char* input = static_cast<const char*>(buffer.input);
+  int32_t size = Xt::Audio::GetSampleAttributes(Mix.sample).size;
+  os->write(input, buffer.frames * size);
 }
 
-int CaptureSimpleMain(int argc, char** argv) {
-
+int CaptureSimpleMain() 
+{
   Xt::Audio audio("", nullptr, nullptr);  
-  auto system = Xt::Audio::SetupToSystem(Xt::Setup::ConsumerAudio);
+  Xt::System system = Xt::Audio::SetupToSystem(Xt::Setup::ConsumerAudio);
   std::unique_ptr<Xt::Service> service = Xt::Audio::GetService(system);
-  if(!service)
-    return 0;  
-
+  if(!service) return 0;  
   std::unique_ptr<Xt::Device> device = service->OpenDefaultDevice(false);
-  if(!device || !device->SupportsFormat(Format))
-    return 0;
-
-  Xt::BufferSize size = device->GetBufferSize(Format);
+  if(!device || !device->SupportsFormat(Format)) return 0;
+  double bufferSize = device->GetBufferSize(Format).current;
+  Xt::StreamParams params(Format, true, bufferSize, Capture, nullptr);
   std::ofstream recording("xt-audio.raw", std::ios::out | std::ios::binary);
-  std::unique_ptr<Xt::Stream> stream = device->OpenStream(Format, true, size.current, Capture, nullptr, &recording);
+  std::unique_ptr<Xt::Stream> stream = device->OpenStream(params, &recording);
   stream->Start();
-#if _WIN32
-  Sleep(1000);
-#else
-  usleep(1000 * 1000);
-#endif
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   stream->Stop();
   return 0;
 }
--- src/cpp-sample/RenderSimple.cpp
+++ src/cpp-sample/RenderSimple.cpp
@@ -1,34 +1,38 @@
 #define _USE_MATH_DEFINES 1
+#include <XtCpp.hpp>
 #include <cmath>
 #include <chrono>
 #include <thread>
 #include <climits>
 #include <iostream>
-#include <XtCpp.hpp>
 
 static float _phase = 0.0f;
 static const float Frequency = 440.0f;
-static const Xt::Format Format(Xt::Mix(44100, Xt::Sample::Float32), Xt::Channels(0, 0, 1, 0));
+static const Xt::Channels Channels(0, 0, 1, 0);
+static const Xt::Mix Mix(44100, Xt::Sample::Float32);
+static const Xt::Format Format(Mix, Channels);
+
+static float NextSample()
+{
+  _phase += Frequency / Mix.rate;
+  if (_phase >= 1.0f) _phase = -1.0f;
+  return std::sinf(2.0f * _phase * M_PI);
+}
 
 static void Render(const Xt::Stream& stream, const Xt::Buffer& buffer, void* user)
 {
   float* output = static_cast<float*>(buffer.output);
-  for(int f = 0; f < buffer.frames; f++) 
-  {
-    _phase += Frequency / Format.mix.rate;
-    if(_phase >= 1.0f) _phase = -1.0f;
-    output[f] = std::sinf(2.0f * _phase * M_PI);
-  }
+  for (int f = 0; f < buffer.frames; f++) output[f] = NextSample();
 }
 
-int RenderSimpleMain() 
+int RenderSimpleMain()
 {
   Xt::Audio audio("", nullptr, nullptr);
   Xt::System system = Xt::Audio::SetupToSystem(Xt::Setup::ConsumerAudio);
   std::unique_ptr<Xt::Service> service = Xt::Audio::GetService(system);
-  if(!service) return 0;
+  if (!service) return 0;
   std::unique_ptr<Xt::Device> device = service->OpenDefaultDevice(true);
-  if(!device || !device->SupportsFormat(Format)) return 0;
+  if (!device || !device->SupportsFormat(Format)) return 0;
   double bufferSize = device->GetBufferSize(Format).current;
   Xt::StreamParams params(Format, true, bufferSize, Render, nullptr);
   std::unique_ptr<Xt::Stream> stream = device->OpenStream(params, nullptr);
