f1708f350b0560303256cee2fc3148b1338c05d4
sjoerd <sjoerdvankreel@gmail.com>
2020-11-18 14:28:36 +0100

    Copy headers to output.

--- build/build.bat
+++ build/build.bat
@@ -13,6 +13,12 @@ if !errorlevel! neq 0 exit /b !errorlevel!
 cd ..
 copy ..\dist\core\xt\Debug\xt-core.dll ..\dist\cpp\sample\Debug\xt-core.dll
 copy ..\dist\core\xt\Release\xt-core.dll ..\dist\cpp\sample\Release\xt-core.dll
+if not exist ..\dist\cpp\xt (mkdir ..\dist\cpp\xt)
+xcopy ..\src\cpp\xt ..\dist\cpp\xt /s /q /y
+if !errorlevel! neq 0 exit /b !errorlevel!
+if not exist ..\dist\core\xt\xt\Release\xt (mkdir ..\dist\core\xt\xt\Release\xt)
+echo f | xcopy ..\src\core\xt\xt\XtAudio.h ..\dist\core\xt\Release\xt\XtAudio.h /s /q /y /f
+if !errorlevel! neq 0 exit /b !errorlevel!
 
 REM java
 cd java\xt
