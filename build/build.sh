7305d73adbe22640af85b4418511d2d8fcedd52c
sjoerd <sjoerdvankreel@gmail.com>
2020-11-18 17:25:39 +0100

    Linux build script.

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+# native
+mkdir -p native/linux/debug
+cd native/linux/debug
+cmake ../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ENABLE_PULSE_AUDIO="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3" >/dev/null
+make >/dev/null
+cd ../../..
+mkdir -p native/linux/release
+cd native/linux/release
+cmake ../../.. -DCMAKE_BUILD_TYPE=Release -DXT_ENABLE_PULSE_AUDIO="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3" >/dev/null
+make >/dev/null
+cd ../../..
+
 exit
 
 # Check necessary environment variables.
