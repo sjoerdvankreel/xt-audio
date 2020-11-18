e8036494c094213604acfc072ac385eb50e551c2
sjoerd <sjoerdvankreel@gmail.com>
2020-11-18 22:14:09 +0100

    Change core project structure take 6.

---
+++ src/core/xt/xt/audio/XtStream.cpp
@@ -0,0 +1,50 @@
+#include <xt/audio/XtStream.h>
+#include <xt/private/Private.hpp>
+
+XtFormat const* XT_CALL 
+XtStreamGetFormat(XtStream const* s) 
+{
+  XT_ASSERT(s != nullptr);
+  return &s->format;
+}
+
+void XT_CALL 
+XtStreamDestroy(XtStream* s) 
+{ 
+  XT_ASSERT(XtiCalledOnMainThread());
+  delete s;
+}
+
+XtError XT_CALL 
+XtStreamStop(XtStream* s) 
+{
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(XtiCalledOnMainThread());
+  return XtiCreateError(s->GetSystem(), s->Stop());
+}
+
+XtError XT_CALL 
+XtStreamStart(XtStream* s) 
+{
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(XtiCalledOnMainThread());
+  return XtiCreateError(s->GetSystem(), s->Start());
+}
+
+XtError XT_CALL 
+XtStreamGetFrames(XtStream const* s, int32_t* frames) 
+{
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(frames != nullptr);
+  *frames = 0;
+  return XtiCreateError(s->GetSystem(), s->GetFrames(frames));
+}
+
+XtError XT_CALL 
+XtStreamGetLatency(XtStream const* s, XtLatency* latency)
+{
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(latency != nullptr);
+  memset(latency, 0, sizeof(XtLatency));
+  return XtiCreateError(s->GetSystem(), s->GetLatency(latency));
+}
