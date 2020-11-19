6bbe4a45ebc215cac1dc9987b75e7807d75b09f6
sjoerd <sjoerdvankreel@gmail.com>
2020-11-19 20:16:39 +0100

    Change core project structure take 8.

--- src/core/xt/xt/XtAudio.cpp
+++ src/core/xt/xt/XtAudio.cpp
@@ -150,36 +150,6 @@ void XT_CALL XtAudioInit(const char* id, void* window, XtOnError onError) {
 
 // ---- service ----
 
-XtCapabilities XT_CALL XtServiceGetCapabilities(const XtService* s) {
-  XT_ASSERT(s != nullptr);
-  return s->GetCapabilities();
-}
-
-XtError XT_CALL XtServiceGetDeviceCount(const XtService* s, int32_t* count) {
-  XT_ASSERT(s != nullptr);
-  XT_ASSERT(count != nullptr);
-  XT_ASSERT(XtiCalledOnMainThread());
-  *count = 0;
-  return XtiCreateError(s->GetSystem(), s->GetDeviceCount(count));
-}
-
-XtError XT_CALL XtServiceOpenDevice(const XtService* s, int32_t index, XtDevice** device) {
-  XT_ASSERT(index >= 0);
-  XT_ASSERT(s != nullptr);
-  XT_ASSERT(device != nullptr);
-  XT_ASSERT(XtiCalledOnMainThread());
-  *device = nullptr;
-  return XtiCreateError(s->GetSystem(), s->OpenDevice(index, device));
-}
-
-XtError XT_CALL XtServiceOpenDefaultDevice(const XtService* s, XtBool output, XtDevice** device) {
-  XT_ASSERT(s != nullptr);
-  XT_ASSERT(device != nullptr);
-  XT_ASSERT(XtiCalledOnMainThread());
-  *device = nullptr;
-  return XtiCreateError(s->GetSystem(), s->OpenDefaultDevice(output, device));
-}
-
 XtError XT_CALL XtServiceAggregateStream(const XtService* s, const XtAggregateStreamParams* params, void* user, XtStream** stream) {
 
   XT_ASSERT(s != nullptr);
---
+++ src/core/xt/xt/audio/XtService.cpp
@@ -0,0 +1,40 @@
+#include <xt/audio/XtService.h>
+#include <xt/private/Private.hpp>
+
+XtCapabilities XT_CALL
+XtServiceGetCapabilities(XtService const* s)
+{
+  XT_ASSERT(s != nullptr);
+  return s->GetCapabilities();
+}
+
+XtError XT_CALL
+XtServiceGetDeviceCount(XtService const* s, int32_t* count)
+{
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(count != nullptr);
+  XT_ASSERT(XtiCalledOnMainThread());
+  *count = 0;
+  return XtiCreateError(s->GetSystem(), s->GetDeviceCount(count));
+}
+
+XtError XT_CALL
+XtServiceOpenDevice(XtService const* s, int32_t index, XtDevice** device)
+{
+  XT_ASSERT(index >= 0);
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(device != nullptr);
+  XT_ASSERT(XtiCalledOnMainThread());
+  *device = nullptr;
+  return XtiCreateError(s->GetSystem(), s->OpenDevice(index, device));
+}
+
+XtError XT_CALL
+XtServiceOpenDefaultDevice(XtService const* s, XtBool output, XtDevice** device)
+{
+  XT_ASSERT(s != nullptr);
+  XT_ASSERT(device != nullptr);
+  XT_ASSERT(XtiCalledOnMainThread());
+  *device = nullptr;
+  return XtiCreateError(s->GetSystem(), s->OpenDefaultDevice(output, device));
+}
