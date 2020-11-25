d37a3c526e0692adf69aeb3db9fce707e856e124
sjoerd <sjoerdvankreel@gmail.com>
2020-11-25 15:15:59 +0100

    Change core project structure - connect to java.

--- src/java/xt/xt/audio/XtAudio.java
+++ src/java/xt/xt/audio/XtAudio.java
@@ -2,7 +2,6 @@ package xt.audio;
 
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
-import com.sun.jna.ptr.IntByReference;
 
 import xt.audio.CoreCallbacks.XtOnError;
 import xt.audio.CoreEnums.XtSample;
@@ -11,47 +10,24 @@ import xt.audio.CoreEnums.XtSystem;
 import xt.audio.CoreStructs.XtAttributes;
 import xt.audio.CoreStructs.XtErrorInfo;
 import xt.audio.CoreStructs.XtVersion;
-import java.util.Arrays;
 
-public final class XtAudio implements AutoCloseable {
+public final class XtAudio {
 
     static { Native.register(Utility.LIBRARY); }
-    private static native void XtAudioTerminate();
     private static native XtVersion.ByValue XtAudioGetVersion();
-    private static native Pointer XtAudioGetService(XtSystem system);
     private static native XtSystem XtAudioSetupToSystem(XtSetup setup);
     private static native XtErrorInfo.ByValue XtAudioGetErrorInfo(long error);
-    private static native void XtAudioGetSystems(int[] buffer, IntByReference size);
-    private static native void XtAudioInit(String id, Pointer window, XtOnError onError);
     private static native XtAttributes.ByValue XtAudioGetSampleAttributes(XtSample sample);
+    private static native Pointer XtAudioInit(String id, Pointer window, XtOnError onError);
 
     private XtAudio() {}
-    static XtOnError _onError;
-
-    @Override public void close() { XtAudioTerminate(); }
     public static XtVersion getVersion() { return XtAudioGetVersion(); }
     public static XtErrorInfo getErrorInfo(long error) { return XtAudioGetErrorInfo(error); }
     public static XtSystem setupToSystem(XtSetup setup) { return XtAudioSetupToSystem(setup); }
     public static XtAttributes getSampleAttributes(XtSample sample) { return XtAudioGetSampleAttributes(sample); }
 
-    public static XtService getService(XtSystem system) {
-        Pointer service = XtAudioGetService(system);
-        return service == Pointer.NULL? null: new XtService(service);
-    }
-
-    public static AutoCloseable init(String id, Pointer window, XtOnError onError) {
-        _onError = onError;
+    public static XtPlatform init(String id, Pointer window, XtOnError onError) {
         XtAudioInit(id, window, onError);
-        return new XtAudio();
-    }
-
-    public static XtSystem[] getSystems() {
-        var mapper = new XtTypeMapper();
-        var size = new IntByReference();
-        XtAudioGetSystems(null, size);
-        var result = new int[size.getValue()];
-        XtAudioGetSystems(result, size);
-        var converter = mapper.getFromNativeConverter(XtSystem.class);
-        return Arrays.stream(result).mapToObj(s -> converter.fromNative(s, null)).toArray(XtSystem[]::new);
+        return new XtPlatform(XtAudioInit(id, window, onError), onError);
     }
 }
---
+++ src/java/xt/xt/audio/XtPlatform.java
@@ -0,0 +1,37 @@
+package xt.audio;
+
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+
+import xt.audio.CoreCallbacks.XtOnError;
+import xt.audio.CoreEnums.XtSystem;
+import java.util.Arrays;
+
+public final class XtPlatform implements AutoCloseable {
+
+    static { Native.register(Utility.LIBRARY); }
+    private static native void XtPlatformDestroy(Pointer p);
+    private static native Pointer XtPlatformGetService(Pointer p, XtSystem system);
+    private static native void XtPlatformGetSystems(Pointer p, int[] buffer, IntByReference size);
+
+    final Pointer _p;
+    final XtOnError _onError;
+    XtPlatform(Pointer p, XtOnError onError) { _p = p; _onError = onError; }
+    @Override public void close() { XtPlatformDestroy(_p); }
+
+    public XtService getService(XtSystem system) {
+        Pointer s = XtPlatformGetService(_p, system);
+        return s == Pointer.NULL? null: new XtService(s);
+    }
+
+    public XtSystem[] getSystems() {
+        var mapper = new XtTypeMapper();
+        var size = new IntByReference();
+        XtPlatformGetSystems(_p, null, size);
+        var result = new int[size.getValue()];
+        XtPlatformGetSystems(_p, result, size);
+        var converter = mapper.getFromNativeConverter(XtSystem.class);
+        return Arrays.stream(result).mapToObj(s -> converter.fromNative(s, null)).toArray(XtSystem[]::new);
+    }
+}
