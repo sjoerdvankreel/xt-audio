775c49112ef441207040f14fe90b7d7f336c6276
sjoerd <sjoerdvankreel@gmail.com>
2020-12-12 14:08:58 +0100

    Restructure dsound, take 2.

--- src/core/xt/xt/dsound/Private.hpp
+++ src/core/xt/xt/dsound/Private.hpp
@@ -2,5 +2,13 @@
 #define XT_DSOUND_PRIVATE_HPP
 #if XT_ENABLE_DSOUND
 
+#include <xt/api/public/Enums.h>
+#include <xt/private/Shared.hpp>
+
+char const* 
+XtiGetDSoundFaultText(XtFault fault);
+XtCause 
+XtiGetDSoundFaultCause(XtFault fault);
+
 #endif // XT_ENABLE_DSOUND
 #endif // XT_DSOUND_PRIVATE_HPP
