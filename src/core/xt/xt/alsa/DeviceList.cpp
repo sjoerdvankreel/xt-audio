9b536ae39f9a4cf1ac4b41660c0b80f8b34cacdb
sjoerd <sjoerdvankreel@gmail.com>
2020-12-12 13:18:36 +0100

    Restructure alsa, take 2.

--- src/core/xt/xt/alsa/DeviceList.cpp
+++ src/core/xt/xt/alsa/DeviceList.cpp
@@ -1,5 +1,34 @@
 #if XT_ENABLE_ALSA
 #include <xt/alsa/Shared.hpp>
 #include <xt/alsa/Private.hpp>
+#include <xt/private/Shared.hpp>
+
+AlsaDeviceList::
+AlsaDeviceList(void** hints):
+_hints(hints) { } 
+
+AlsaDeviceList::
+~AlsaDeviceList()
+{ XT_ASSERT(snd_device_name_free_hint(_hints) == 0); }
+
+XtFault
+AlsaDeviceList::GetCount(int32_t* count) const
+{
+}
+
+XtFault
+AlsaDeviceList::GetDefaultId(XtBool output, XtBool* valid, char* buffer, int32_t* size) const
+{
+}
+  
+XtFault 
+AlsaDeviceList::GetId(int32_t index, char* buffer, int32_t* size) const
+{  
+}
+
+XtFault
+AsioDeviceList::GetName(char const* id, char* buffer, int32_t* size) const
+{
+}
 
 #endif // XT_ENABLE_ALSA
--- src/core/xt/xt/alsa/Shared.hpp
+++ src/core/xt/xt/alsa/Shared.hpp
@@ -1,6 +1,19 @@
 #ifndef XT_ALSA_SHARED_HPP
 #define XT_ALSA_SHARED_HPP
 #if XT_ENABLE_ALSA
+#include <xt/api/private/Device.hpp>
+#include <xt/api/private/Stream.hpp>
+#include <xt/api/private/Service.hpp>
+#include <xt/api/private/DeviceList.hpp>
+
+struct AlsaDeviceList:
+public XtDeviceList
+{
+  void** const _hints;
+  ~AlsaDeviceList();
+  AlsaDeviceList(void** hints);
+  XT_IMPLEMENT_DEVICE_LIST(ALSA);
+};
 
 #endif // XT_ENABLE_ALSA
 #endif // XT_ALSA_SHARED_HPP
