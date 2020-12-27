8fccdf5fe4f22f6450c9828b05aaf63b0eba9042
sjoerd <sjoerdvankreel@gmail.com>
2020-12-27 13:23:20 +0100

    restore x86 support take 3

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
-mkdir -p native/linux/debug
-cd native/linux/debug
-cmake ../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3" >/dev/null
+mkdir -p native/linux/"$1"/debug
+cd native/linux/"$1"/debug
+cmake ../../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ARCH="$1" -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$2" -DXT_ENABLE_ALSA="$3" -DXT_ENABLE_JACK="$4" >/dev/null
 make >/dev/null
-cd ../../..
-mkdir -p native/linux/release
-cd native/linux/release
-cmake ../../.. -DCMAKE_BUILD_TYPE=Release -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3" >/dev/null
+cd ../../../..
+
+mkdir -p native/linux/"$1"/release
+cd native/linux/"$1"/release
+cmake ../../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ARCH="$1" -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$2" -DXT_ENABLE_ALSA="$3" -DXT_ENABLE_JACK="$4" >/dev/null
 make >/dev/null
-cd ../../..
+cd ../../../..
+
 cp ../dist/core/xt/Debug/libxt-core.so ../dist/cpp/sample/Debug/libxt-core.so || :
 cp ../dist/core/xt/Release/libxt-core.so ../dist/cpp/sample/Release/libxt-core.so || :
