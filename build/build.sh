6fcbfe4f8433b778557b8f3366e6dab41c3e5cc3
sjoerd <sjoerdvankreel@gmail.com>
2020-12-27 13:24:58 +0100

    fixes

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ cd ../../../..
 
 mkdir -p native/linux/"$1"/release
 cd native/linux/"$1"/release
-cmake ../../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ARCH="$1" -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$2" -DXT_ENABLE_ALSA="$3" -DXT_ENABLE_JACK="$4" >/dev/null
+cmake ../../../.. -DCMAKE_BUILD_TYPE=Release -DXT_ARCH="$1" -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$2" -DXT_ENABLE_ALSA="$3" -DXT_ENABLE_JACK="$4" >/dev/null
 make >/dev/null
 cd ../../../..
 
