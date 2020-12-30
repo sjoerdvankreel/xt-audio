8240fd53610e58594d07ba15852a88592326129d
sjoerd <sjoerdvankreel@gmail.com>
2020-12-30 17:09:13 +0100

    cleanup gui take 16

--- src/cli/gui/Callbacks/OnRender.cs
+++ src/cli/gui/Callbacks/OnRender.cs
@@ -2,140 +2,118 @@
 
 namespace Xt
 {
-    public class OnRender : OnBuffer
+    public unsafe class OnRender : OnBuffer
     {
-        private double phase;
-        private static readonly double Frequency = 440.0;
+        static readonly double Frequency = 440.0;
+
+        double _phase;
         public OnRender(OnBufferParams @params) : base(@params) { }
 
         protected override void ProcessBuffer(XtStream stream, in XtBuffer buffer, XtSafeBuffer safe)
-        {/*
+        {
+            var frames = buffer.frames;
+            var format = stream.GetFormat();
             for (int f = 0; f < frames; f++)
             {
-
                 int ival;
-                double val = Math.Sin(phase * 2.0 * Math.PI);
-                phase += Frequency / format.mix.rate;
-                if (phase >= 1.0)
-                    phase = -1.0;
+                double val = Math.Sin(_phase * 2.0 * Math.PI);
+                _phase += Frequency / format.mix.rate;
+                if (_phase >= 1.0) _phase = -1.0;
+                if (Params.Native && Params.Interleaved) ProcessInterleavedNative(in format, f, val, buffer.output);
+                if (Params.Native && !Params.Interleaved) ProcessNonInterleavedNative(in format, f, val, buffer.output);
+                if (!Params.Native && Params.Interleaved) ProcessInterleavedSafe(in format, f, val, safe.GetOutput());
+                if (!Params.Native && !Params.Interleaved) ProcessNonInterleavedSafe(in format, f, val, safe.GetOutput());
+            }
+        }
 
-                if (!native)
+        void ProcessInterleavedSafe(in XtFormat format, int frame, double val, Array output)
+        {
+            int ival;
+            for (int c = 0; c < format.channels.outputs; c++)
+            {
+                int pos = frame * format.channels.outputs + c;
+                switch (format.mix.sample)
                 {
-                    if (interleaved)
-                        for (int c = 0; c < format.channels.outputs; c++)
-                        {
-                            int pos = f * format.channels.outputs + c;
-                            switch (format.mix.sample)
-                            {
-                            case XtSample.UInt8:
-                            ((byte[])output)[pos] = (byte)((val * 0.5 + 0.5) * byte.MaxValue);
-                            break;
-                            case XtSample.Int16:
-                            ((short[])output)[pos] = (short)(val * short.MaxValue);
-                            break;
-                            case XtSample.Int24:
-                            ival = (int)(val * int.MaxValue);
-                            ((byte[])output)[pos * 3] = (byte)((ival & 0x0000FF00) >> 8);
-                            ((byte[])output)[pos * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
-                            ((byte[])output)[pos * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
-                            break;
-                            case XtSample.Int32:
-                            ((int[])output)[pos] = (int)(val * int.MaxValue);
-                            break;
-                            case XtSample.Float32:
-                            ((float[])output)[pos] = (float)val;
-                            break;
-                            default:
-                            break;
-                            }
-                        }
-                    else
-                        for (int c = 0; c < format.channels.outputs; c++)
-                        {
-                            switch (format.mix.sample)
-                            {
-                            case XtSample.UInt8:
-                            ((byte[][])output)[c][f] = (byte)((val * 0.5 + 0.5) * byte.MaxValue);
-                            break;
-                            case XtSample.Int16:
-                            ((short[][])output)[c][f] = (short)(val * short.MaxValue);
-                            break;
-                            case XtSample.Int24:
-                            ival = (int)(val * int.MaxValue);
-                            ((byte[][])output)[c][f * 3] = (byte)((ival & 0x0000FF00) >> 8);
-                            ((byte[][])output)[c][f * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
-                            ((byte[][])output)[c][f * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
-                            break;
-                            case XtSample.Int32:
-                            ((int[][])output)[c][f] = (int)(val * int.MaxValue);
-                            break;
-                            case XtSample.Float32:
-                            ((float[][])output)[c][f] = (float)val;
-                            break;
-                            default:
-                            break;
-                            }
-                        }
-                } else
-                {
-                    if (interleaved)
-                        for (int c = 0; c < format.channels.outputs; c++)
-                        {
-                            int pos = f * format.channels.outputs + c;
-                            switch (format.mix.sample)
-                            {
-                            case XtSample.UInt8:
-                            ((byte*)(IntPtr)output)[pos] = (byte)((val * 0.5 + 0.5) * byte.MaxValue);
-                            break;
-                            case XtSample.Int16:
-                            ((short*)(IntPtr)output)[pos] = (short)(val * short.MaxValue);
-                            break;
-                            case XtSample.Int24:
-                            ival = (int)(val * int.MaxValue);
-                            ((byte*)(IntPtr)output)[pos * 3] = (byte)((ival & 0x0000FF00) >> 8);
-                            ((byte*)(IntPtr)output)[pos * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
-                            ((byte*)(IntPtr)output)[pos * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
-                            break;
-                            case XtSample.Int32:
-                            ((int*)(IntPtr)output)[pos] = (int)(val * int.MaxValue);
-                            break;
-                            case XtSample.Float32:
-                            ((float*)(IntPtr)output)[pos] = (float)val;
-                            break;
-                            default:
-                            break;
-                            }
-                        }
-                    else
-                        for (int c = 0; c < format.channels.outputs; c++)
-                        {
-                            switch (format.mix.sample)
-                            {
-                            case XtSample.UInt8:
-                            ((byte**)(IntPtr)output)[c][f] = (byte)((val * 0.5 + 0.5) * byte.MaxValue);
-                            break;
-                            case XtSample.Int16:
-                            ((short**)(IntPtr)output)[c][f] = (short)(val * short.MaxValue);
-                            break;
-                            case XtSample.Int24:
-                            ival = (int)(val * int.MaxValue);
-                            ((byte**)(IntPtr)output)[c][f * 3] = (byte)((ival & 0x0000FF00) >> 8);
-                            ((byte**)(IntPtr)output)[c][f * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
-                            ((byte**)(IntPtr)output)[c][f * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
-                            break;
-                            case XtSample.Int32:
-                            ((int**)(IntPtr)output)[c][f] = (int)(val * int.MaxValue);
-                            break;
-                            case XtSample.Float32:
-                            ((float**)(IntPtr)output)[c][f] = (float)val;
-                            break;
-                            default:
-                            break;
-                            }
-                        }
+                case XtSample.Float32: ((float[])output)[pos] = (float)val; break;
+                case XtSample.Int32: ((int[])output)[pos] = (int)(val * int.MaxValue); break;
+                case XtSample.Int16: ((short[])output)[pos] = (short)(val * short.MaxValue); break;
+                case XtSample.UInt8: ((byte[])output)[pos] = (byte)((val * 0.5 + 0.5) * byte.MaxValue); break;
+                case XtSample.Int24:
+                ival = (int)(val * int.MaxValue);
+                ((byte[])output)[pos * 3] = (byte)((ival & 0x0000FF00) >> 8);
+                ((byte[])output)[pos * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
+                ((byte[])output)[pos * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
+                break;
+                default: throw new InvalidOperationException();
                 }
             }
-            */
+        }
+
+        void ProcessInterleavedNative(in XtFormat format, int frame, double val, IntPtr output)
+        {
+            int ival;
+            for (int c = 0; c < format.channels.outputs; c++)
+            {
+                int pos = frame * format.channels.outputs + c;
+                switch (format.mix.sample)
+                {
+                case XtSample.Float32: ((float*)output)[pos] = (float)val; break;
+                case XtSample.Int32: ((int*)output)[pos] = (int)(val * int.MaxValue); break;
+                case XtSample.Int16: ((short*)output)[pos] = (short)(val * short.MaxValue); break;
+                case XtSample.UInt8: ((byte*)output)[pos] = (byte)((val * 0.5 + 0.5) * byte.MaxValue); break;
+                case XtSample.Int24:
+                ival = (int)(val * int.MaxValue);
+                ((byte*)output)[pos * 3] = (byte)((ival & 0x0000FF00) >> 8);
+                ((byte*)output)[pos * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
+                ((byte*)output)[pos * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
+                break;
+                default: throw new InvalidOperationException();
+                }
+            }
+        }
+
+        void ProcessNonInterleavedSafe(in XtFormat format, int frame, double val, Array output)
+        {
+            int ival;
+            for (int c = 0; c < format.channels.outputs; c++)
+            {
+                switch (format.mix.sample)
+                {
+                case XtSample.Float32: ((float[][])output)[c][frame] = (float)val; break;
+                case XtSample.Int32: ((int[][])output)[c][frame] = (int)(val * int.MaxValue); break;
+                case XtSample.Int16: ((short[][])output)[c][frame] = (short)(val * short.MaxValue); break;
+                case XtSample.UInt8: ((byte[][])output)[c][frame] = (byte)((val * 0.5 + 0.5) * byte.MaxValue); break;
+                case XtSample.Int24:
+                ival = (int)(val * int.MaxValue);
+                ((byte[][])output)[c][frame * 3] = (byte)((ival & 0x0000FF00) >> 8);
+                ((byte[][])output)[c][frame * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
+                ((byte[][])output)[c][frame * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
+                break;
+                default: throw new InvalidOperationException();
+                }
+            }
+        }
+
+        void ProcessNonInterleavedNative(in XtFormat format, int frame, double val, IntPtr output)
+        {
+            int ival;
+            for (int c = 0; c < format.channels.outputs; c++)
+            {
+                switch (format.mix.sample)
+                {
+                case XtSample.Float32: ((float**)output)[c][frame] = (float)val; break;
+                case XtSample.Int32: ((int**)output)[c][frame] = (int)(val * int.MaxValue); break;
+                case XtSample.Int16: ((short**)output)[c][frame] = (short)(val * short.MaxValue); break;
+                case XtSample.UInt8: ((byte**)output)[c][frame] = (byte)((val * 0.5 + 0.5) * byte.MaxValue); break;
+                case XtSample.Int24:
+                ival = (int)(val * int.MaxValue);
+                ((byte**)output)[c][frame * 3] = (byte)((ival & 0x0000FF00) >> 8);
+                ((byte**)output)[c][frame * 3 + 1] = (byte)((ival & 0x00FF0000) >> 16);
+                ((byte**)output)[c][frame * 3 + 2] = (byte)((ival & 0xFF000000) >> 24);
+                break;
+                default: throw new InvalidOperationException();
+                }
+            }
         }
     }
 }
