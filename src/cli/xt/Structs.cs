82356ba5c461beae3f6a71997cec9fe0bfb55e9e
sjoerd <sjoerdvankreel@gmail.com>
2021-01-20 18:56:44 +0100

    Bugfix.

--- src/cli/xt/Structs.cs
+++ src/cli/xt/Structs.cs
@@ -106,8 +106,8 @@ namespace Xt
         public int count;
         int _isFloat;
         int _isSigned;
-        public bool isFloat { get => _isFloat != 0; set => _isFloat = value ? 0 : 1; }
-        public bool isSigned { get => _isSigned != 0; set => _isSigned = value ? 0 : 1; }
+        public bool isFloat => _isFloat != 0;
+        public bool isSigned => _isSigned != 0;
     }
 
     [StructLayout(LayoutKind.Sequential)]
