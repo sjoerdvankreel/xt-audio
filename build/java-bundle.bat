3f27ec7e21655cd627451e5dfc8640bf2ac52621
sjoerd <sjoerdvankreel@gmail.com>
2021-01-25 21:04:16 +0100

    prepare for release

--- build/java-bundle.bat
+++ build/java-bundle.bat
@@ -1,8 +1,8 @@
 @echo off
 cd ..\dist\java\xt\target\
-gpg -ab xt.audio-1.8-SNAPSHOT.pom
-gpg -ab xt.audio-1.8-SNAPSHOT.jar
-gpg -ab xt.audio-1.8-SNAPSHOT-sources.jar
-gpg -ab xt.audio-1.8-SNAPSHOT-javadoc.jar
-jar -cvf xt.audio-1.8-SNAPSHOT-bundle.jar xt.audio-1.8-SNAPSHOT.pom xt.audio-1.8-SNAPSHOT.jar xt.audio-1.8-SNAPSHOT-sources.jar xt.audio-1.8-SNAPSHOT-javadoc.jar xt.audio-1.8-SNAPSHOT.pom.asc xt.audio-1.8-SNAPSHOT.jar.asc xt.audio-1.8-SNAPSHOT-sources.jar.asc xt.audio-1.8-SNAPSHOT-javadoc.jar.asc
+gpg -ab xt.audio-1.8.pom
+gpg -ab xt.audio-1.8.jar
+gpg -ab xt.audio-1.8-sources.jar
+gpg -ab xt.audio-1.8-javadoc.jar
+jar -cvf xt.audio-1.8-bundle.jar xt.audio-1.8.pom xt.audio-1.8.jar xt.audio-1.8-sources.jar xt.audio-1.8-javadoc.jar xt.audio-1.8.pom.asc xt.audio-1.8.jar.asc xt.audio-1.8-sources.jar.asc xt.audio-1.8-javadoc.jar.asc
 cd ..\..\..\..\build
