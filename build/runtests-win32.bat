07f3216638dfd1b128970b8be94c525ed9955a42
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-14 18:59:45 +0100

    Update version number.

--- build/runtests-win32.bat
+++ build/runtests-win32.bat
@@ -4,7 +4,7 @@ set /p ok="C++ x86 sample..."
 set /p ok="C++ x64 sample..."
 ..\dist\cpp\sample\x64\Release\xt-sample.exe
 set /p ok="Java sample..."
-java -jar ..\dist\java\sample\target\xt.sample-1.9.jar
+java -jar ..\dist\java\sample\target\xt.sample-2.0.jar
 set /p ok=".NET Framework sample..."
 ..\dist\net\sample\Release\net48\Xt.Sample.exe
 set /p ok=".NET Core sample..."
