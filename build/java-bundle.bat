ffcd3505075986415ad2a748964be984a43e22d2
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2021-12-24 12:25:33 +0100

    Revert "Tryout java splitting debug/release packages, take 2."
    This reverts commit 8f86977a50439037693ebe15dcf67a162c3ce6ee.

--- build/java-bundle.bat
+++ build/java-bundle.bat
@@ -1,8 +1,8 @@
 @echo off
-cd ..\dist\java\xt\Release\target\
+cd ..\dist\java\xt\target\
 gpg -ab xt.audio-2.0-SNAPSHOT.pom
 gpg -ab xt.audio-2.0-SNAPSHOT.jar
 gpg -ab xt.audio-2.0-SNAPSHOT-sources.jar
 gpg -ab xt.audio-2.0-SNAPSHOT-javadoc.jar
 jar -cvf xt.audio-2.0-SNAPSHOT-bundle.jar xt.audio-2.0-SNAPSHOT.pom xt.audio-2.0-SNAPSHOT.jar xt.audio-2.0-SNAPSHOT-sources.jar xt.audio-2.0-SNAPSHOT-javadoc.jar xt.audio-2.0-SNAPSHOT.pom.asc xt.audio-2.0-SNAPSHOT.jar.asc xt.audio-2.0-SNAPSHOT-sources.jar.asc xt.audio-2.0-SNAPSHOT-javadoc.jar.asc
-cd ..\..\..\..\..\build
+cd ..\..\..\..\build
