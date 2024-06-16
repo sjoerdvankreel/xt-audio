fbadaba5402e97adf5330573c477660a518d3b00
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-06-16 19:13:19 +0200

    take 6

--- build/test-win32.cmd
+++ build/test-win32.cmd
@@ -1,41 +1,41 @@
 @echo off
 
 echo C++ debug...
-..\dist\cpp\sample\%1\Debug\xt-sample.exe "%2"
+..\dist\cpp\sample\Debug\xt-sample.exe "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo C++ release...
-..\dist\cpp\sample\%1\Release\xt-sample.exe "%2"
+..\dist\cpp\sample\Release\xt-sample.exe "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo NetFX debug...
-..\dist\net\sample\Debug\net48\Xt.Sample.exe "%2"
+..\dist\net\sample\Debug\net48\Xt.Sample.exe "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo NetFX release...
-..\dist\net\sample\Release\net48\Xt.Sample.exe "%2"
+..\dist\net\sample\Release\net48\Xt.Sample.exe "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo NetCore debug...
-dotnet ..\dist\net\sample\Debug\net6.0\Xt.Sample.dll "%2"
+dotnet ..\dist\net\sample\Debug\net6.0\Xt.Sample.dll "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo NetCore release...
-dotnet ..\dist\net\sample\Release\net6.0\Xt.Sample.dll "%2"
+dotnet ..\dist\net\sample\Release\net6.0\Xt.Sample.dll "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo Java debug...
-java -jar ..\dist\java\sample\debug\target\xt.sample.debug-2.2.jar "%2"
+java -jar ..\dist\java\sample\debug\target\xt.sample.debug-2.2.jar "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
 
 echo Java release...
-java -jar ..\dist\java\sample\release\target\xt.sample-2.2.jar "%2"
+java -jar ..\dist\java\sample\release\target\xt.sample-2.2.jar "%1"
 if %errorlevel% neq 0 exit /b !errorlevel!
 echo:
