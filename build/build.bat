cc8d2e4e3a710b53b620c6f58aa01d011bab5823
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-06-16 17:26:29 +0200

    take 3

--- build/build.bat
+++ build/build.bat
@@ -1,26 +1,19 @@
 @echo off
 setLocal enableDelayedExpansion
 
-set archs[0]=x86
-set archs[1]=x64
-set vsarchs[0]=Win32
-set vsarchs[1]=x64
-
 REM native binaries
-for /L %%A in (0, 1, 1) do (
-  if not exist native\win32\!archs[%%A]! (mkdir native\win32\!archs[%%A]!)
-  cd native\win32\!archs[%%A]!
-  cmake ..\..\.. -G"Visual Studio 17 2022" -A !vsarchs[%%A]! -DXT_ARCH=!archs[%%A]! -DXT_ENABLE_ALSA=0 -DXT_ENABLE_JACK=0 -DXT_ENABLE_PULSE=0 -DXT_ENABLE_DSOUND=%1 -DXT_ENABLE_WASAPI=%2 -DXT_ENABLE_ASIO=%3 -DXT_ASIOSDK_DIR=%4 -DXT_ASMJIT_DIR=%5 > NUL
-  if !errorlevel! neq 0 exit /b !errorlevel!
-  msbuild xt-audio.sln /p:Configuration=Debug /verbosity:quiet
-  if !errorlevel! neq 0 exit /b !errorlevel!
-  msbuild xt-audio.sln /p:Configuration=Release /verbosity:quiet
-  if !errorlevel! neq 0 exit /b !errorlevel!
-  cd ..\..\..
-  copy ..\dist\core\xt\!archs[%%A]!\Debug\xt-audio.dll ..\dist\cpp\sample\!archs[%%A]!\Debug\xt-audio.dll
-  copy ..\dist\core\xt\!archs[%%A]!\Debug\xt-audio.pdb ..\dist\cpp\sample\!archs[%%A]!\Debug\xt-audio.pdb
-  copy ..\dist\core\xt\!archs[%%A]!\Release\xt-audio.dll ..\dist\cpp\sample\!archs[%%A]!\Release\xt-audio.dll
-)
+if not exist native\win32 (mkdir native\win32)
+cd native\win32
+cmake ..\.. -G"Visual Studio 17 2022" -A x64 -DXT_ENABLE_ALSA=0 -DXT_ENABLE_JACK=0 -DXT_ENABLE_PULSE=0 -DXT_ENABLE_DSOUND=%1 -DXT_ENABLE_WASAPI=%2 -DXT_ENABLE_ASIO=%3 -DXT_ASIOSDK_DIR=%4 -DXT_ASMJIT_DIR=%5
+if !errorlevel! neq 0 exit /b !errorlevel!
+msbuild xt-audio.sln /p:Configuration=Debug
+if !errorlevel! neq 0 exit /b !errorlevel!
+msbuild xt-audio.sln /p:Configuration=Release
+if !errorlevel! neq 0 exit /b !errorlevel!
+cd ..\..
+copy ..\dist\core\xt\Debug\xt-audio.dll ..\dist\cpp\sample\Debug\xt-audio.dll
+copy ..\dist\core\xt\Debug\xt-audio.pdb ..\dist\cpp\sample\Debug\xt-audio.pdb
+copy ..\dist\core\xt\Release\xt-audio.dll ..\dist\cpp\sample\Release\xt-audio.dll
 
 REM native includes
 if not exist ..\dist\cpp\xt\include (mkdir ..\dist\cpp\xt\include)
@@ -46,6 +39,8 @@ if !errorlevel! neq 0 exit /b !errorlevel!
 call mvn -f pom.debug.xml -q install
 if !errorlevel! neq 0 exit /b !errorlevel!
 cd ..\..
+
+goto :EOF
 
 REM net
 cd net
