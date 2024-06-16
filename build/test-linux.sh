a68d40e4b4fb0896394310ac8625fdc0a495a819
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-06-16 17:56:07 +0200

    drop x86 support take 2

--- build/test-linux.sh
+++ build/test-linux.sh
@@ -2,38 +2,33 @@
 set -e
 
 echo "C++ debug..."
-../dist/cpp/sample/$1/Debug/xt-sample $2
+../dist/cpp/sample/$1/Debug/xt-sample $1
 echo $'\n'
 
 echo "C++ release..."
-../dist/cpp/sample/$1/Release/xt-sample $2
+../dist/cpp/sample/$1/Release/xt-sample $1
 echo $'\n'
 
 echo "Mono debug..."
-mono ../dist/net/sample/Debug/net48/Xt.Sample.exe $2
+mono ../dist/net/sample/Debug/net48/Xt.Sample.exe $1
 echo $'\n'
 
 echo "Mono release..."
-mono ../dist/net/sample/Release/net48/Xt.Sample.exe $2
+mono ../dist/net/sample/Release/net48/Xt.Sample.exe $1
 echo $'\n'
 
 echo "Java debug..."
-java -jar ../dist/java/sample/debug/target/xt.sample.debug-2.2.jar $2
+java -jar ../dist/java/sample/debug/target/xt.sample.debug-2.2.jar $1
 echo $'\n'
 
 echo "Java release..."
-java -jar ../dist/java/sample/release/target/xt.sample-2.2.jar $2
+java -jar ../dist/java/sample/release/target/xt.sample-2.2.jar $1
 echo $'\n'
 
-if [[ "$1" == "x86" ]]
-then
-  exit 0;
-fi
-
 echo "NetCore debug..."
-dotnet ../dist/net/sample/Debug/net6.0/Xt.Sample.dll $2
+dotnet ../dist/net/sample/Debug/net6.0/Xt.Sample.dll $1
 echo $'\n'
 
 echo "NetCore release..."
-dotnet ../dist/net/sample/Release/net6.0/Xt.Sample.dll $2
+dotnet ../dist/net/sample/Release/net6.0/Xt.Sample.dll $1
 echo $'\n'
