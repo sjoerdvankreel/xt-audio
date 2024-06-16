0b90b94de16d98bc2e8b65dc206f606cb3b39e33
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-06-16 17:06:45 +0200

    take 2

--- build/build.sh
+++ build/build.sh
@@ -3,14 +3,14 @@ set -e
 
 mkdir -p native/linux/debug
 cd native/linux/debug
-cmake ../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3" >/dev/null
-make >/dev/null
+cmake ../../.. -DCMAKE_BUILD_TYPE=Debug -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3"
+make
 cd ../../..
 cp ../dist/core/xt/Debug/libxt-audio.so ../dist/cpp/sample/Debug/libxt-audio.so || :
 
 mkdir -p native/linux/release
 cd native/linux/release
-cmake ../../.. -DCMAKE_BUILD_TYPE=Release -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3" >/dev/null
-make >/dev/null
+cmake ../../.. -DCMAKE_BUILD_TYPE=Release -DXT_ENABLE_ASIO=0 -DXT_ENABLE_WASAPI=0 -DXT_ENABLE_DSOUND=0 -DXT_ENABLE_PULSE="$1" -DXT_ENABLE_ALSA="$2" -DXT_ENABLE_JACK="$3"
+make
 cd ../../..
 cp ../dist/core/xt/Release/libxt-audio.so ../dist/cpp/sample/Release/libxt-audio.so || :
